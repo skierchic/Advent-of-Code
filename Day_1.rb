08c4fe145ad1fb1e1989796918d5d5b721b21006
Jenna Hight <jennamhight@gmail.com>
2017-12-03 15:55:34 -0500

    clean up 1st half

--- Day_1.rb
+++ Day_1.rb
@@ -1,7 +1,6 @@
 captcha = File.read("day_1_input.txt").chomp
 puts captcha
 last_digit = captcha[-1]
-puts "last digit is #{last_digit}"
 sum = 0
 captcha.split('').each do |digit|
   if digit == last_digit

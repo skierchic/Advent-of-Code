44cfd54c6ca8dafdf437ea8e7259e53bbe4666c9
Jenna Hight <jennamhight@gmail.com>
2017-12-03 15:52:13 -0500

    complete 1st half of day 1 challenge

--- Day_1.rb
+++ Day_1.rb
@@ -1,6 +1,7 @@
-puts "enter captcha"
-captcha = gets.chomp
+captcha = File.read("day_1_input.txt").chomp
+puts captcha
 last_digit = captcha[-1]
+puts "last digit is #{last_digit}"
 sum = 0
 captcha.split('').each do |digit|
   if digit == last_digit

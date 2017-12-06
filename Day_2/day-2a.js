f3497beb36a16e7019214d862df489665f0a8020
Jenna Hight <jennamhight@gmail.com>
2017-12-06 16:15:22 -0500

    adding comments to Day 2a challenge

--- Day_2/day-2a.js
+++ Day_2/day-2a.js
@@ -7,15 +7,19 @@ let rows = input.split('\n')
 //loop through rows and find the amoun to add to the checksum
 let checksum = 0
 rows.forEach((row) => {
-  //split rows into numbers
+  //split rows into numbers initialize min and max to first number
   nums = row.split('\t')
   let max = parseInt(nums[0])
   let min = parseInt(nums[0])
+
+  //loop through the numbers to find the max and min for each row
   nums.forEach((num) => {
     num = parseInt(num)
     max = num > max ? num : max
     min = num < min ? num : min
   })
+
+  //add the difference of the max and min to the checksum
   checksum += max - min
 })
 console.log(`checksum = ${checksum}`)

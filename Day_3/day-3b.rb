530861c685349054f4c2602a2aa0de8ceab02655
Jenna Hight <jennamhight@gmail.com>
2017-12-17 23:53:22 -0500

    plan out methods with pseudo code

--- Day_3/day-3b.rb
+++ Day_3/day-3b.rb
@@ -10,6 +10,20 @@ def make_spiral(input)
   return [spiral, center, center]
 end
 
+def make_loop(spiral, x, y)
+  #follow spiral path and evalute the location along the way
+  #break out when a number is greater than the input value?
+  # loop do
+  #   x += 1
+  #   spiral[x][y] = evaluate_location_value(spiral, x, y)
+  #   break if spiral[x][y + 1] == 0
+  # end
 
+end
+
+def evaluate_location_value(spiral, x, y)
+  #evaluate the value of the given location in the given spiral by adding
+  #the values for all neighbors and return
+end
 
 make_spiral(25)

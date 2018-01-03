8b883b5b236193abaa11c9b681d533b23f682167
Jenna Hight <jennamhight@gmail.com>
2018-01-02 23:31:15 -0500

    day 6a code

---
+++ Day_6/day_6a.rb
@@ -0,0 +1,32 @@
+require "pry"
+banks = File.read("day_6_input.txt").chomp.split
+banks.map! { |bank| bank.to_i }
+
+def redistribute(banks)
+  total_blocks_to_redistribute = banks.max
+  max_bank = banks.find_index(total_blocks_to_redistribute)
+  blocks_to_add_to_each_bank = total_blocks_to_redistribute / banks.count
+  banks_that_get_extra_block = total_blocks_to_redistribute % banks.count
+
+  if banks.count - max_bank >= banks_that_get_extra_block
+    after_ending_bank = banks.count - 1
+    before_ending_bank = banks_that_get_extra_block - (banks.count - max_bank)
+  else
+    after_ending_bank = max_bank + banks_that_get_extra_block
+    before_ending_bank = 0
+  end
+
+  banks.map!.with_index do |bank, index|
+    if index < before_ending_bank
+      bank += blocks_to_add_to_each_bank + 1
+    elsif before_ending_bank <= index && index <= max_bank
+      bank += blocks_to_add_to_each_bank
+    elsif max_bank < index && index <= after_ending_bank
+      bank += blocks_to_add_to_each_bank + 1
+    end
+    puts bank, index
+  end
+
+  return banks
+end
+binding.pry
